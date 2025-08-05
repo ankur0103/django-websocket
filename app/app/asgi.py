63273b34d21603ebd36216c6bea52965d614dd6e
Ankur Kushwaha <ankur@dsider.app>
2025-08-05 09:49:06 +0530

    merge urls and route.py

--- app/app/asgi.py
+++ app/app/asgi.py
@@ -12,7 +12,7 @@ import structlog
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from chat.routing import websocket_urlpatterns
+from chat.urls import websocket_urlpatterns
 from chat.lifespan import LifespanMiddleware
 
 logger = structlog.get_logger(__name__)
--- app/chat/urls.py
+++ app/chat/urls.py
@@ -1,11 +1,17 @@
-from django.urls import path
-from . import views
+from django.urls import path, re_path
+from . import views, consumers
 from .views import pre_shutdown
 
 app_name = 'chat'
 
+# HTTP URL patterns
 urlpatterns = [
     path("", views.chat_home, name="home"),
     path("info", views.websocket_info, name="websocket_info"),
     path('pre-shutdown/', pre_shutdown),
+]
+
+# WebSocket URL patterns
+websocket_urlpatterns = [
+    re_path(r'^ws/chat/$', consumers.ChatConsumer.as_asgi())
 ] 
